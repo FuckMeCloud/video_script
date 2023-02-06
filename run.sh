67a438483daf0d6c1e3f12976b12d088a65e1234
guoruize <guoruize@360.cn>
2023-02-07 00:51:19 +0800

    init

---
+++ run.sh
@@ -0,0 +1,3 @@
+
+youtube-dl https://www.youtube.com/playlist?list=PLBvB3hPv5qV1JLI9UkBAevoSXy9zQomeH --format mp4
+# youtube-dl https://www.youtube.com/playlist?list=PLBvB3hPv5qV181ZfPs3S1gJP5uDcqmnis --format mp4
