e4aac044faa96093cdd8adf99ab5f5d9852c3606
Aaron3S <chenyang@fit2cloud.com>
2023-02-16 11:14:25 +0800

    add dep

--- run.sh
+++ run.sh
@@ -1,3 +1,2 @@
 
 youtube-dl https://www.youtube.com/playlist?list=PLBvB3hPv5qV1JLI9UkBAevoSXy9zQomeH --format mp4
-# youtube-dl https://www.youtube.com/playlist?list=PLBvB3hPv5qV181ZfPs3S1gJP5uDcqmnis --format mp4
